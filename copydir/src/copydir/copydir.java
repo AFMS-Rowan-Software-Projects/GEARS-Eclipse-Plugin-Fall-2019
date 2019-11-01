ba4ac72ab24669651d509b7afcc14b5b1352e36b
Syndicist <31106886+Syndicist@users.noreply.github.com>
2019-11-01 11:06:00 -0400

    identify variant files and directories
    files with a dot(.) prefix

--- copydir/src/copydir/copydir.java
+++ copydir/src/copydir/copydir.java
@@ -3,6 +3,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.regex.Pattern;
 
 public class copydir 
 {
@@ -15,39 +16,55 @@ public class copydir
 		cloneDir(f, n);
 	}
 	
+	
 	public static void cloneDir(File from, File to) throws IOException
 	{
 		File[] files = from.listFiles();
+		String pattern = "[.]+.";
 		for(int i = 0; i < files.length; i++)
 		{
 			File temp = new File(to, files[i].getName());
 			if(files[i].isDirectory())
 			{
-				//TODO: if temp is projected directory, modify name and apply projection
-				//else, create the directory and continue copying recursively
-				temp.mkdir();
-				cloneDir(files[i], temp);
+				if(Pattern.matches(pattern, files[i].getName()))	//if temp is projected directory, modify name and apply projection
+				{
+					temp = new File(to, temp.getName().substring(1));
+					temp.mkdir();
+					//TODO: apply projection
+				}
+				else	//else, create the directory and continue copying recursively
+				{
+					temp.mkdir();
+					cloneDir(files[i], temp);
+				}
 			}
 			else
 				try
 				{
-					//TODO: if temp is projected file, modify name and apply projection
-					//else, just copy the file as below
-					temp.createNewFile();
-					var source = Paths.get(files[i].toString());
-			        var dest = Paths.get(temp.toString());
-
-			        try (var fis = Files.newInputStream(source);
-			             var fos = Files.newOutputStream(dest)) {
-
-			            byte[] buffer = new byte[1024];
-			            int length;
-
-			            while ((length = fis.read(buffer)) > 0) {
-
-			                fos.write(buffer, 0, length);
-			            }
-			        }
+					if(Pattern.matches(pattern, files[i].getName()))	//if temp is projected file, modify name and apply projection
+					{
+						temp = new File(to, temp.getName().substring(1));
+						temp.createNewFile();
+						//TODO: apply projection
+					}
+					else	//else, just copy the file as below
+					{					
+						temp.createNewFile();
+						var source = Paths.get(files[i].toString());
+				        var dest = Paths.get(temp.toString());
+	
+				        try (var fis = Files.newInputStream(source);
+				             var fos = Files.newOutputStream(dest)) {
+	
+				            byte[] buffer = new byte[1024];
+				            int length;
+	
+				            while ((length = fis.read(buffer)) > 0) {
+	
+				                fos.write(buffer, 0, length);
+				            }
+				        }
+					}
 				}
 				catch (IOException e)
 				{
