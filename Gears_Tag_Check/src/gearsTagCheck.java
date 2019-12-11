a2a4fb3d22ff61ab9b02a04a02359bda201db060
mcgroarym5 <55498628+mcgroarym5@users.noreply.github.com>
2019-12-10 21:04:14 -0500

    Add files via upload

--- Gears_Tag_Check/src/gearsTagCheck.java
+++ Gears_Tag_Check/src/gearsTagCheck.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.ArrayList;
 //import java.nio.file.Files;
 import java.util.Scanner;
 
@@ -13,8 +14,12 @@ public class gearsTagCheck {
 		File codeFile = new File("ex");
 		File outputFile;
 		printFile(codeFile);
+		ArrayList<String> tags = new ArrayList<String>();
+		tags.add("//Delete");
+		//Dummy tag "//Delete" is for the test files, but for the actual files the
+		// arrayList of Strings for the tags would be determined from the logic files
 		System.out.println("\n");
-		outputFile = convert(codeFile, "testing");
+		outputFile = convert(codeFile, "testing", tags);
 		System.out.println("\n");
 		printFile(outputFile);
 		
@@ -30,7 +35,7 @@ public class gearsTagCheck {
 		sc.close();
 	}
 	
-	public static File convert(File codeFile, String newFileName) throws IOException {
+	public static File convert(File codeFile, String newFileName, ArrayList<String> tags) throws IOException {
 		
 		Scanner sc = new Scanner(codeFile);
 		File outputFile = new File(newFileName);
@@ -44,23 +49,30 @@ public class gearsTagCheck {
 		
 		while (sc.hasNextLine()) {
 			String currLine = sc.nextLine();
-			if(currLine.contains("//Delete")) {
-				if(deleteTagStatus == 0) {
-					deleteTagStatus = 1;
-					System.out.println("Found Tag, deleting lines");
+			//iterates through the current set of deletion tags provided by the tags arraylist
+			for(int i=0; i<tags.size(); i++) {
+				if(currLine.contains(tags.get(i))) {
+					if(deleteTagStatus == 0) {
+						//If found the deletion tag, start the deletion process
+						deleteTagStatus = 1;
+						System.out.println("Found Tag, deleting lines");
+					} else {
+						//If found the deletion tag, end the deletion process
+						deleteTagStatus = 0;
+						System.out.println("Found Tag, saving lines");
+					}
 				} else {
-					deleteTagStatus = 0;
-					System.out.println("Found Tag, saving lines");
-				}
-			} else {
-				if(deleteTagStatus == 0) {
-					System.out.println("Keep this line");
-					fw.write(currLine + "\n");
-				} else {
-					System.out.println("Delete this line");
+					if(deleteTagStatus == 0) {
+						//if not deleting, write the line to the new file
+						fw.write(currLine + "\n");
+					} else {
+						//if deleting, ignore the line and move to the next one
+						System.out.println("Delete this line");
+					}
 				}
 			}
 		}
+			
 		
 		fw.close();
 		sc.close();
